8648d856f01908ed785c55d7ad263bce64b94ed2
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-04-25 21:02:53 +0100

    move finding shortest neighbour methods into ExplorerGraph

--- src/main/java/student/ExplorerGraph.java
+++ src/main/java/student/ExplorerGraph.java
@@ -25,6 +25,16 @@ public class ExplorerGraph {
     tileNodes.add(node);
   }
   
+  public void addTileNodeToMap(TileNode node, List<TileNode> neighbours) {
+	  explorerMap.put(node, neighbours);
+  }
+  
+  /**
+   * 
+   * @param start
+   * @param end
+   */
+  
   public void connectNode(final TileNode start, final TileNode end) {
     List<TileNode> nodeList = explorerMap.get(start);
       if (nodeList == null) {
@@ -34,21 +44,42 @@ public class ExplorerGraph {
     nodeList.add(end);
   }
   
-  /* 
-   * returns an arraylist of neighbours that have not been visited
+  /**
+   *  Returns the list of TileNodes of the neighbours to the current location
+   *  that have not yet been visited
+   *  @param neighbours - the list of neighbours to the current node
+   *  @return ArrayList of unvisited neighbours
    */
+ 
   public List<TileNode> getUnvisitedNeighbours(final TileNode node) {
     final List<TileNode> neighbours = explorerMap.get(node);
     final List<TileNode> unvisitedNeighbours = new ArrayList<TileNode>();
     if (neighbours != null) {
       for (int i = 0; i < neighbours.size(); i++) {
         final TileNode temp = neighbours.get(i);
-        if (tileNodes.contains(node)) {
+        if (!temp.getVisited()) {
           unvisitedNeighbours.add(temp);
         }
       }
     }
     return unvisitedNeighbours;
   }
-  
+  /**
+   *  Returns the NodeStatus of the neighbour with the shortest distance to the orb
+   *  @param neighbours - the list of unvisited neighbours to the current node
+   *  @return TileNode with the shorted distance to orb
+   */
+  public TileNode returnShortestNeighbour(List<TileNode> neighbours) {
+    TileNode shortestNeighbour = neighbours.get(0);
+    int shortestDistance = shortestNeighbour.getDistance();
+    for(TileNode n : neighbours) {
+      int distanceComparison = n.getDistance();
+      if (shortestDistance > distanceComparison) {
+        shortestNeighbour = n;
+        shortestDistance = distanceComparison;
+      }
+    }
+    
+    return shortestNeighbour;
+  } 
 }
