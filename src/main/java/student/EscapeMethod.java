2117cc1a1e1dda6b93feb2dadf6eb92825582f97
Hannah Spencer <hspenc03@dcs.bbk.ac.uk>
2017-05-06 11:29:42 +0100

    Create escape method searching for route with map

--- src/main/java/student/EscapeMethod.java
+++ src/main/java/student/EscapeMethod.java
@@ -1,7 +1,16 @@
 package student;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import game.Edge;
+import game.EscapeState;
 import game.Node;
 import game.Tile;
 
@@ -11,12 +20,44 @@ public class EscapeMethod {
 	int exitRow;
 	int exitColumn;
 	Set<Node> exitNeighbours;
+	Set<Edge> exitEdges;
+	Node exit;
+	List<Node> route = new ArrayList<>();
+	Map<Node, Set<Node>> nodesAndFriends = new HashMap<>();
+	Map<Node, Integer> nodesAndDistances = new HashMap<>();
 	
 	public void pinpointExit(Node exit) {
+		this.exit = exit;
 		exitTile = exit.getTile();
 		exitRow = exitTile.getRow();
 		exitColumn = exitTile.getColumn();
 		exitNeighbours = exit.getNeighbours();
+		exitEdges = exit.getExits();
 	}
+	
+	public void scanforRoute(Node current) {
+		route.add(exit);
+		Set<Node> neighbours = nodesAndFriends.get(current);
+		for (Node n : neighbours) {
+			for (Edge e : exitEdges) {
+				if (n.getExits().contains(e)) {
+					route.add(current);
+				} else {
+					route.add(n);
+					scanforRoute(n);
+				}
+		}
+	  }
+	}
+	
+	public void updateMaps(Node current, Set<Node> neighbours) {
+	  nodesAndFriends.put(current, neighbours);
+	  for (Node n : neighbours) {
+		  Set<Node> nodeNeighbours = n.getNeighbours();
+		  updateMaps(n, nodeNeighbours);
+	  }
+        
+	}
+
 
 }
